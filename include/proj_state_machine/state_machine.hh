5916740596c12df755a66fb0a27c9348839bed2e
Shota Iuchi <shotaiuchi.develop@gmail.com>
2017-02-01 22:23:27 +0900

    [state_machine]create

---
+++ include/proj_state_machine/state_machine.hh
@@ -0,0 +1,7 @@
+#ifndef __STATE_MACHINE_HH__
+#define __STATE_MACHINE_HH__
+
+namespace state_machine {
+};
+
+#endif // __STATE_MACHINE_HH__
---
+++ src/proj_state_machine/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "proj_state_machine/state_machine.hh"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "__state_machine__" << std::endl;
+    return 0;
+}
